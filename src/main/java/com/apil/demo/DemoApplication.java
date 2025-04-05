594103d2dae4c8cf396e16f602521c9c0796ffe9
Apil Khadka <apilkhadka2020@gmail.com>
2025-04-05 13:48:19 +0545

    home set

--- src/main/java/com/apil/demo/DemoApplication.java
+++ src/main/java/com/apil/demo/DemoApplication.java
@@ -2,14 +2,22 @@ package com.apil.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 
+@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "<Strong>Its a API!</Strong>, Please check at /api/v1/student";
     }
 
 }
